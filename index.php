67ae69795ba0b5d2abd80e230c94a037bd3667d1
olaf <csaba.gaal@integralvision.hu>
2023-10-01 18:42:32 +0200

    added index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+﻿<?php
+
+use PhpD64\Disk;
+
+require_once 'vendor/autoload.php';
+require_once 'functions.php';
+
+$disk = new Disk();
+$disk->loadFromFile('test.d64');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>PHP-D64 test page</title>
+    <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+    <div class="d64-directory">
+        <div class="header">
+            <div>0</div>
+            <div class="inverse">"<?= petscii_to_html(utf8_encode($disk->getName())) ?>"</div>
+            <div class="inverse"><?= utf8_encode($disk->getHeader()) ?></div>
+        </div>
+        <div class="files">
+            <?php foreach ($disk->getDirectory() as $file) { ?>
+                <div class="file">
+                    <div class="size"><?= $file->getSize() ?></div>
+                    <div class="name">"<?= str_replace(' ', '&nbsp;', petscii_to_html($file->getName())); ?>"</div>
+                    <div class="type"><?= $file->getFileType() ?></div>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+    <?= $disk->getFreeBlocks() ?> BLOCKS FREE.
+</body>
+</html>
